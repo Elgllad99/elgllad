e9fbfeace6d8a51dda75f928563b97748fb64633
Elgllad99 <71010851+Elgllad99@users.noreply.github.com>
2020-10-16 03:55:11 +0200

    Update sensitive.php

--- db/sensitive.php
+++ db/sensitive.php
@@ -4,9 +4,9 @@ define('EMAIL','EMAIL');
 define('PASSWORD','PASSWORD');
 
 
-define('DB_HOST', '127.0.0.1:3306');
-define('DB_USER', 'root');
+define('DB_HOST', '1');
+define('DB_USER', '1');
 define('DB_PASS', '');
-define('DB_NAME', 'registration');
+define('DB_NAME', '1');
 
 ?>
